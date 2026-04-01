5e59e69ba4fec3e0de3ff9d052b959c77e617938
andoni.eguiluz <andoni.eguiluz@MEDALT10448.deusto.es>
2026-04-01 19:17:20 +0200

    Runner naves

---
+++ Clase2026/src/tema1c/resueltos/runnerNaves/Rotable.java
@@ -0,0 +1,10 @@
+package tema1c.resueltos.runnerNaves;
+
+public interface Rotable {
+
+	/** Rota el objeto
+	 * @param rot	Ángulo a rotar (en radianes)
+	 */
+	public void rota( double rot );
+	
+}
