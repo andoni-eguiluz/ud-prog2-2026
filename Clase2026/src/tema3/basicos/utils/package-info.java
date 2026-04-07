2b092946ceeb01d9ee0e16df09f3ba47ec94de71
andoni.eguiluz <andoni.eguiluz@MEDALT10448.deusto.es>
2026-04-07 16:25:21 +0200

    Código tema 3

--- Clase2026/src/tema3/basicos/utils/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Paquete de utilidades para visualizar datos de comportamiento en ejecución de Java Collections.
- * No necesario para programación ni desarrollo de la asignatura Programación II
- */
-package tema3.basicos.utils;
