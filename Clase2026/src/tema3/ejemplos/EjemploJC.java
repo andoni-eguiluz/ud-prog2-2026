bfea36883cdecf084147f3422907ff96102c2156
andoni.eguiluz <andoni.eguiluz@MEDALT10448.deusto.es>
2026-04-13 07:41:56 +0200

    Ejemplo de películas para practicar JC

---
+++ Clase2026/src/tema3/ejemplos/EjemploJC.java
@@ -0,0 +1,50 @@
+package tema3.ejemplos;
+
+/** Clase de ejemplo de uso de Java Collections
+ * @author andoni.eguiluz at ingenieria.deusto.es
+ */
+public class EjemploJC {
+	public static void main(String[] args) {
+		pruebasJCconStrings();
+		// pruebasJCconClasesPropias();
+	}
+
+	private static String[] peliculasOscars2023 = {
+			"Oppenheimer", "Oppenheimer", "Oppenheimer", "Pobres criaturas",   // 4 principales
+			"Oppenheimer", "Los que se quedan",  // Act secundarios
+			"La zona de interés",   // Internacional
+			"Anatomía de una caída",  // Guión original
+			"American fiction", // Guión adaptado
+			"Oppenheimer", "Oppenheimer", "La zona de interés", "Oppenheimer",  // Fotografía, montaje, sonido, banda sonora
+			"Pobres criaturas", "Pobres criaturas", "Pobres criaturas",  // Producción, vestuario, maquillaje
+			"Godzilla: minus one",  // Efectos visuales
+			"Barbie",  // Canción
+			"El chico y la garza", // Animación
+			"20 días en Mariupol", // Documental
+			"La maravillosa historia de Henry Sugar", "The last repair shop", "War is over" // Cortos
+		};
+	
+	private static String[] premioOscar2023 = {
+	        "Mejor película", "Mejor dirección", "Mejor actor", "Mejor actriz",
+	        "Mejor actor de reparto", "Mejor actriz de reparto",
+	        "Mejor película internacional",
+	        "Mejor guión original",
+	        "Mejor guión adaptado",
+	        "Mejor fotografía", "Mejor montaje", "Mejor sonido", "Mejor banda sonora",
+	        "Mejor diseño de producción", "Mejor vestuario", "Mejor maquillaje y peluquería",
+	        "Mejores efectos visuales",
+	        "Mejor canción original",
+	        "Mejor película de animación",
+	        "Mejor documental",
+	        "Mejor cortometraje de ficción", "Mejor cortometraje documental", "Mejor cortometraje de animación"
+	};
+		
+	private static void pruebasJCconStrings() {
+		// TODO
+	}
+	
+	private static void pruebasJCconClasesPropias() {
+		// TODO
+	}
+	
+}
