b8f0226f304450ef89c1d977b1bab0311a2e844c
andoni.eguiluz <andoni.eguiluz@MEDALT10448.deusto.es>
2026-04-26 01:23:16 +0200

    Ejemplo de ficheros de texto de objetos

---
+++ Clase2026/src/tema4/ejemplos/objetosATexto/Empleado.java
@@ -0,0 +1,29 @@
+package tema4.ejemplos.objetosATexto;
+
+public class Empleado extends Persona {
+	private String cargo;
+
+	public Empleado(int dni, char letraDNI, String nombre, String apellidos, int anyoNacimiento, String cargo) throws PersonaException {
+		super(dni, letraDNI, nombre, apellidos, anyoNacimiento);
+		this.cargo = cargo;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " [" + cargo + "]";
+	}
+
+	// TODO Tarea 4
+	// public String aLinea() {
+	
+	// TODO Tarea 7
+	// public static Empleado crearDesde(String[] partes) throws PersonaException
+}
